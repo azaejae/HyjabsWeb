8b7189e8b39173cbf6b647111ca61543841bfc60
azaejae <azaejae@gmail.com>
2013-11-04 12:40:38 +0700

    add index file

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @author azaejae
+ * @copyright 2013
+ */
+
+?>
+    <!DOCTYPE HTML>
+    <html>
+        <head>
+            <title>Hyjabs - Inspiring Your Beauty by Hijab</title>
+            <style>
+                body{
+                    background-color: pink;
+                }
+                
+            </style>
+        </head>
+        <body>
+            <form>
+               <input type="text" name="nama" placeholder="Your Name" /><br />
+               <input type="text" name="email" placeholder="your E-mail" /><br />
+               <input type="password" name="password" placeholder="Password" /><br />
+               <input type="button" name="daftar" value="Daftar" />
+            </form>
+        </body>
+    </html>
+    
+<?php
+
+?>
